e46cae8dad05d4212939dadf64f58efd13860bb1
jiofi2018 <63831048+jiofi2018@users.noreply.github.com>
2022-11-20 20:18:44 +0530

    cookies 1add

--- App.js
+++ App.js
@@ -50,6 +50,12 @@ app.use(cookieParser());
 // app.use(session({ secret: 'secret' }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(function(req,res,next){
+  res.header('Access-Control-Allow-Credentials',true);
+  res.header('Access-Control-Allow-Origin','http://localhost:3000');
+  res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+  res.header('Access-Control-Allow-Headers','X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+})
 
 // Routes
 const router = require('./routes/user')
